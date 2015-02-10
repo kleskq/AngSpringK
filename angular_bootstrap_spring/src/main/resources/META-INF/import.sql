16a9b2759cd5b45cf6748c3b4d14f6ed912e314c
Dariusz Kobuszewski <dariusz.kobuszewski@doskomp.lodz.pl>
2015-02-10 01:28:18 +0100

    Zamockowanie newsa

--- angular_bootstrap_spring/src/main/resources/META-INF/import.sql
+++ angular_bootstrap_spring/src/main/resources/META-INF/import.sql
@@ -4,5 +4,5 @@ insert into Userrole (Role,UserId) values ('ROLE_ADMIN','2');
 insert into Userrole (Role,UserId) values ('ROLE_USER','1');
 insert into Category  (CategoryName) values ('fast note');
 insert into Category  (CategoryName) values ('programming');
-insert into News (link,NewsTitle,NewsText,NewsImageUrl,CreateDate,UserId,news,MINUSRATING,PLUSRATING) values ('1233','title1','text','google.pl','2014-12-27 15:40:46.959','1','1','2','2');
+insert into News (link,NewsTitle,NewsText,NewsImageUrl,CreateDate,UserId,news,MINUSRATING,PLUSRATING) values ('1233','Smieszny tytuł','Treść posta też bardzo zabawna, lorem ipsum dolor sit amed ounertnekr jewfsdkjf fds ffewr','https://pbs.twimg.com/media/B6eIhnkIYAAQdFz.jpg:large','2014-12-27 15:40:46.959','1','1','2','2');
 insert into Rate(Rating,UserId,NewsId) values ('TRUE','1','1');
